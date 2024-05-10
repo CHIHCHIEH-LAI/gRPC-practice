2eb4a3f434c276d0e8d7a3fa4563415cc49b4510
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-05-10 13:22:05 +0800

    chore: Update client and server code to use asyncio and grpc.aio

--- src/client.py
+++ src/client.py
@@ -1,12 +1,14 @@
-import grpc
+import grpc.aio
+import asyncio
 import messages_pb2
 import messages_pb2_grpc
 
-def run():
-    with grpc.insecure_channel('localhost:50051') as channel:
+async def run():
+    async with grpc.aio.insecure_channel('localhost:50051') as channel:
         stub = messages_pb2_grpc.GreetingServiceStub(channel)
-        response = stub.SayHello(messages_pb2.HelloRequest(name='Alice'))
-        print("Greeting client received: " + response.message)
+        for i in range(100):
+            response = await stub.SayHello(messages_pb2.HelloRequest(name='Alice'+str(i)))
+            print("Greeting client received: " + response.message)
 
 if __name__ == '__main__':
-    run()
+    asyncio.run(run())
--- src/server.py
+++ src/server.py
@@ -1,19 +1,21 @@
-from concurrent import futures
-import grpc
+import asyncio
+import grpc.aio
 import messages_pb2
 import messages_pb2_grpc
+import time
 
 class GreetingService(messages_pb2_grpc.GreetingServiceServicer):
 
-    def SayHello(self, request, context):
+    async def SayHello(self, request, context):
+        time.sleep(1)
         return messages_pb2.HelloReply(message='Hello, {}!'.format(request.name))
 
-def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+async def serve():
+    server = grpc.aio.server()
     messages_pb2_grpc.add_GreetingServiceServicer_to_server(GreetingService(), server)
     server.add_insecure_port('[::]:50051')
-    server.start()
-    server.wait_for_termination()
+    await server.start()
+    await server.wait_for_termination()
 
 if __name__ == '__main__':
-    serve()
+    asyncio.run(serve())
